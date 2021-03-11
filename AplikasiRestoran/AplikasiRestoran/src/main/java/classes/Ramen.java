33e5d00a2f28d0d2070b0d6d12093791d95f84f6
T²N <75306063+Tatuns@users.noreply.github.com>
2021-03-11 08:02:13 +0700

    Delete AplikasiRestoran/AplikasiRestoran directory

--- AplikasiRestoran/AplikasiRestoran/src/main/java/classes/Ramen.java
+++
@@ -1,8 +0,0 @@
-package classes;
-public class Ramen extends Menu{
-	public Ramen(String nama_ramen, double harga) {
-            setNama_menu(nama_ramen);
-            setHarga(harga);
-            setKategori("Ramen");
-        }
-}
